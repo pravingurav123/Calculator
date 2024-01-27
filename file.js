0804cb33ab6da485913352336517c06a51f5edcf
pravin gurav <pravingurav630@gmail.com>
2024-01-27 23:03:34 +0530

    web

---
+++ file.js
@@ -0,0 +1,83 @@
+
+function no1() {
+	f1.result.value += f1.n1.value;
+
+}
+function no2() {
+	f1.result.value += f1.n2.value;
+
+}
+
+function no3() {
+	f1.result.value += f1.n3.value;
+
+}
+
+function addtion() {
+	f1.result.value += f1.add.value;
+
+}
+
+function no4() {
+	f1.result.value += f1.n4.value;
+
+}
+function no5() {
+	f1.result.value += f1.n5.value;
+
+}
+function no6() {
+	f1.result.value += f1.n6.value;
+
+}
+
+function substraction() {
+	f1.result.value += f1.sub.value;
+
+}
+
+function no7() {
+	f1.result.value += f1.n7.value;
+
+}
+function no8() {
+	f1.result.value += f1.n8.value;
+
+}
+function no9() {
+	f1.result.value += f1.n9.value;
+
+}
+
+
+function multipliction() {
+	f1.result.value += f1.mult.value;
+
+}
+
+function dotF() {
+	f1.result.value += f1.dot.value;
+
+}
+
+function zero() {
+	f1.result.value += f1.n0.value;
+
+}
+function division() {
+	f1.result.value += f1.div.value;
+
+}
+
+
+function solve() {
+
+	let x = f1.result.value
+	let y = eval(x);
+	f1.result.value = y
+}
+
+function clearScreen() {
+	f1.result.value = ''
+}
+
